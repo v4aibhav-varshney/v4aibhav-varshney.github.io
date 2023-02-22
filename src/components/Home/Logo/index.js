4d24a65b38c8c522046549166125b1cb3565aff4
Vaibhav Varshney <v4aibhav-varshney@users.noreply.github.com>
2023-02-22 16:54:54 +0530

    Fourth commit

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -2,37 +2,37 @@ import './index.scss'
 import LogoS from '../../../assets/images/logo-v.png'
 import { useRef } from 'react'
 import { useEffect } from 'react';
-import gsap from 'gsap-trial';
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin';
+// import gsap from 'gsap-trial';
+// import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin';
 
 const Logo = () => {
     const bgRef = useRef() ;
     const outlineLogoRef = useRef() ;
     const solidLogoRef = useRef() ;
 
-    useEffect(() => {
-        gsap.registerPlugin(DrawSVGPlugin) 
+    // useEffect(() => {
+    //     gsap.registerPlugin(DrawSVGPlugin) 
 
-        gsap.timeline()
-        .to(bgRef.current,{
-            duration: 1,
-            opacity: 1,
-        })
-        .from(outlineLogoRef.current,{
-            drawSVG: 1,
-            duration: 20,
-        })
+    //     gsap.timeline()
+    //     .to(bgRef.current,{
+    //         duration: 1,
+    //         opacity: 1,
+    //     })
+    //     .from(outlineLogoRef.current,{
+    //         drawSVG: 1,
+    //         duration: 20,
+    //     })
 
-        gsap.fromTo(
-            solidLogoRef.current,{
-                opacity: 0,
-            },{
-                opacity: 1,
-                delay: 4,
-                duration: 4,
-            }
-        )
-    },[])
+    //     gsap.fromTo(
+    //         solidLogoRef.current,{
+    //             opacity: 0,
+    //         },{
+    //             opacity: 1,
+    //             delay: 4,
+    //             duration: 4,
+    //         }
+    //     )
+    // },[])
 
     return (
       <div className="logo-container" ref={bgRef}>
